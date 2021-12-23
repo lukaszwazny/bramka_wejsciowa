d146eee90de656140b63ac95ade97466c65d9f28
lukaszwazny <32484689+lukaszwazny@users.noreply.github.com>
2021-12-24 00:36:46 +0100

    funkcja 33

---
+++ azure_functions_integration/Function33/__init__.py
@@ -0,0 +1,43 @@
+import logging
+import json
+
+from shared_code import database
+
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    logging.info('Getting parameters')
+    req_body = req.get_json()
+    name = req_body.get('name')
+
+    if name:
+        try:
+            cur = database.connectPostgres()
+            query = f"""
+                INSERT INTO public."Lesson_type"(
+                name, is_active)
+                VALUES ('{name}', {True});
+            """
+            cur.execute(query)
+            cur.execute("COMMIT")
+
+            return func.HttpResponse(
+                    "OK",
+                    status_code=200,
+                    mimetype='application/json'
+                )
+        
+        except Exception as ex:
+            logging.info('Unknown error')
+            logging.info(ex)
+            return func.HttpResponse(
+                "Unknown error",
+                status_code=400
+            )
+    else:
+        return func.HttpResponse(
+            "Nie podano wszystkich wymaganych danych",
+            status_code=400
+        )
