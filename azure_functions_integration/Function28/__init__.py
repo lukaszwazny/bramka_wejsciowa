2d813cd47edd3337a6039c15fda509ffc445dc8d
lukaszwazny <32484689+lukaszwazny@users.noreply.github.com>
2021-12-24 00:07:46 +0100

    funkcja 28

---
+++ azure_functions_integration/Function28/__init__.py
@@ -0,0 +1,33 @@
+import logging
+import json
+
+from shared_code import database
+from shared_code.helpers import safe_list_get
+
+import azure.functions as func
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    logging.info('Getting parameters')
+    lesson_type_id = req.params.get('lesson_type_id')
+
+    try:
+
+        cur = database.connectPostgres()
+        query = f'SELECT lesson_id, weekday, start_time, end_time FROM public."Lesson" WHERE is_active AND lesson_type_id={lesson_type_id};'
+        lessons = json.dumps(database.getMany(cur, query), default=str, ensure_ascii=False)
+        
+        return func.HttpResponse(
+                lessons,
+                status_code=200,
+                mimetype='application/json'
+            )
+    
+    except Exception as ex:
+        logging.info('Unknown error')
+        logging.info(ex)
+        return func.HttpResponse(
+            "Unknown error",
+            status_code=400
+        )
