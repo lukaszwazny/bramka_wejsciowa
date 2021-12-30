1c7905fc31ed8a3a70222e161f630bda942f96e6
lukaszwazny <32484689+lukaszwazny@users.noreply.github.com>
2021-12-30 18:05:36 +0100

    fun 34

---
+++ azure_functions_integration/Function34/__init__.py
@@ -0,0 +1,45 @@
+import logging
+import json
+import requests
+
+from shared_code import database
+from shared_code.helpers import safe_list_get, get_key, get_url
+from shared_code.getters import get_active_user
+
+import azure.functions as func
+import Function1
+
+
+def main(req: func.HttpRequest) -> func.HttpResponse:
+    logging.info('Python HTTP trigger function processed a request.')
+
+    logging.info('Getting parameters')
+    identificator_nr = req.params.get('identificator_nr')
+
+    if identificator_nr:
+        try:
+            resp = get_active_user(identificator_nr)
+
+            #to do - rozkaz do apki webowej i urządzenia
+            if type(resp) is Exception:
+                raise resp
+            else:
+                resp = json.dumps(resp, default=str, ensure_ascii=False)
+            return func.HttpResponse(
+                resp,
+                status_code=200,
+                mimetype='application/json'
+            )
+        
+        except Exception as ex:
+            logging.info('Unknown error')
+            logging.info(ex)
+            return func.HttpResponse(
+                "Unknown error",
+                status_code=400
+            )
+    else:
+        return func.HttpResponse(
+             "Nie podano nr identyfikatora!",
+             status_code=400
+        )
