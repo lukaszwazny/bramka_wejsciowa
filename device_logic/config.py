a5a5966301e1bc6ec620ac524131f21eec558128
Łukasz Ważny <lukaszwazny98@gmail.com>
2022-01-03 02:38:09 +0100

    first part of main logic

--- device_logic/config.py
+++ device_logic/config.py
@@ -1,5 +1,5 @@
-opening_angle=-8.879040017426007084292689552528771406
-closing_angle=-1.709975946676696989353108872543860109
+opening_angle=-8.879040017426007084292689552528771406 #in degrees
+closing_angle=-1.709975946676696989353108872543860109 #in degrees
 in_reader_port='/dev/ttyTHS1'
 out_reader_port='/dev/ttyS0'
-relay=26
+relay=26 #in BCM mode
--- device_logic/relay.py
+++ device_logic/relay.py
@@ -1,13 +1,26 @@
+import enum
 import RPi.GPIO as GPIO
 import config
 
+bcm_to_tegra = {
+    k: list(GPIO.gpio_pin_data.get_data()[-1]['TEGRA_SOC'].keys())[i] for i, k in enumerate(GPIO.gpio_pin_data.get_data()[-1]['BCM'])
+}
+
 def setup():
-    GPIO.setmode(GPIO.BCM)
+    if GPIO.getmode() == None:
+        GPIO.setmode(GPIO.BCM)
     GPIO.setwarnings(False)
-    GPIO.setup(config.relay, GPIO.OUT, initial=GPIO.HIGH)
+    GPIO.setup(get_pin(config.relay), GPIO.OUT, initial=GPIO.HIGH)
+    print('GPIO setup done!')
 
 def open():
-    GPIO.output(config.relay, GPIO.LOW)
+    GPIO.output(get_pin(config.relay), GPIO.LOW)
 
 def close():
-    GPIO.output(config.relay, GPIO.HIGH)
+    GPIO.output(get_pin(config.relay), GPIO.HIGH)
+
+def clean():
+    GPIO.cleanup()
+
+def get_pin(pin):
+    return config.relay if GPIO.getmode() == GPIO.BCM else bcm_to_tegra.get(config.relay)
